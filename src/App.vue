0983d51443d40a1b4a1f1e5692bae47db0409fc1
MarcoBeraldo <beraldomar@gmail.com>
2022-07-26 17:00:02 +0200

    imported array of albums in data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
 <template>
   <div id="app">
-    <AppHeader/>
+    <AppHeader />
   </div>
 </template>
 
 <script>
-import AppHeader from "./components/AppHeader.vue"
-
+import AppHeader from "./components/AppHeader.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      albums: [],
+    };
+  },
   components: {
-   AppHeader,
-  }
-}
+    AppHeader,
+  },
+  methods: {
+    getAlbums() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((res) => {
+          console.log(res.data);
+          this.albums = res.data.response;
+        });
+    },
+  },
+
+  mounted() {
+    this.getAlbums();
+  },
+};
 </script>
 
 <style lang="scss">
-  *{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  body{
-    background-color: #1E2D3B;
-    height: 100vh;
-  }
+body {
+  background-color: #1e2d3b;
+  height: 100vh;
+}
 </style>
