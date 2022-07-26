cb67588c8f68dd5bffdd47c6b79cc1ee5394ab1d
MarcoBeraldo <beraldomar@gmail.com>
2022-07-26 14:51:39 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
   <div id="app">
-   
+    <AppHeader/>
   </div>
 </template>
 
 <script>
-
+import AppHeader from "./components/AppHeader.vue"
 
 export default {
   name: 'App',
   components: {
-   
+   AppHeader,
   }
 }
 </script>
 
 <style lang="scss">
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
+  body{
+    background-color: #1E2D3B;
+    height: 100vh;
+  }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/7/75/Spotify_icon.png" alt="Logo">
+  </header>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<style scoped>
+header{
+    background-color: #2E3A46;
+    height: 100px;
+    padding: 20px;
+}
+
+header> img{
+    height: 60px;
+}
+</style>
