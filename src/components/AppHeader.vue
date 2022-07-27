30270e8092339eee3b6cf5b3c1365bae48fbda72
MarcoBeraldo <beraldomar@gmail.com>
2022-07-27 14:43:04 +0200

    created static select component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,26 +1,34 @@
 <template>
-  <header>
+  <header id="header">
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/7/75/Spotify_icon.png"
       alt="Logo"
     />
+    <SelectFilter />
   </header>
 </template>
 
 <script>
+import SelectFilter from "../components/SelectFilter.vue";
 export default {
   name: "AppHeader",
+  components: {
+    SelectFilter,
+  },
 };
 </script>
 
 <style scoped>
-header {
+#header {
   background-color: #2e3a46;
   height: 100px;
   padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-header > img {
+#header > img {
   height: 60px;
 }
 </style>
---
+++ src/components/SelectFilter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="form-floating">
+    <select class="form-select" id="floatingSelect">
+      <option selected>Select Genre</option>
+      <option value="rock">Rock</option>
+      <option value="jazz">Jazz</option>
+      <option value="pop">Pop</option>
+      <option value="metal">Metal</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectFilter",
+};
+</script>
+
+<style scoped>
+</style>
